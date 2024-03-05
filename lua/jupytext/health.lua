d98bd8abf9f3d3c6247a5b68a505837322b75068
Luca Gilardi <lucag@icsi.berkeley.edu>
2024-03-04 19:53:40 -0800

    Check that jupytext be installed in a virtualenv-independent way

--- lua/jupytext/health.lua
+++ lua/jupytext/health.lua
@@ -1,10 +1,15 @@
 local M = {}
 
+local is_installed = function(mod)
+  vim.cmd "python3 import pkgutil"
+  vim.cmd "python3 import importlib"
+  return vim.fn.py3eval("importlib.util.find_spec('" .. mod .. "') is not None")
+end
+
 M.check = function()
   vim.health.report_start "jupytext.nvim"
-  vim.fn.system "jupytext --version"
 
-  if vim.v.shell_error == 0 then
+  if is_installed "jupytext" then
     vim.health.report_ok "Jupytext is available"
   else
     vim.health.report_error("Jupytext is not available", "Install jupytext via `pip install jupytext`")
